847ab366fca6bbc1140263916a008977395b63d3
dgleiche <dylan@Dylans-MacBook-Pro.local>
2015-01-08 23:04:38 -0500

    Revert 8b6c2ef..9ef6cdf
    This rolls back to commit 8b6c2ef34ef9730435be3f5e804c32b2824d9cf4.

---
+++ SHS App/info.swift
@@ -0,0 +1,12 @@
+//
+//  info.swift
+//  SHS App
+//
+//  Created by Dylan on 12/23/14.
+//  Copyright (c) 2014 Dylan. All rights reserved.
+//
+
+import Foundation
+
+let u = "dg47072"
+let p = "Spongebob123"
